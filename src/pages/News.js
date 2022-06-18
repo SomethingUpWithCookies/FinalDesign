d0ce5338d7b49c774bc1cc8ad454dc44fb480759
Crosby <gitEmailRespo@gmail.com>
2022-06-18 16:20:21 -0700

    Finishes Up

--- src/pages/News.js
+++ src/pages/News.js
@@ -8,8 +8,14 @@ import {useParams} from "react-router-dom";
 
 import defaultBackground from "../assets/defaultBackground.jpg"
 
+import Renee from "../assets/ReneeTest.jpg";
 import Izzy from "../assets/Izzy.jpg";
-import Renee from "../assets/ReneeTest.jpg"
+import Crosby from "../assets/Crosby.JPG";
+import Siya from "../assets/Siya.jpg";
+import Ryan from "../assets/Ryan.png";
+import Meher from "../assets/Meher.jpg";
+import JJ from "../assets/JJ.png";
+
 
 
 function News(){
@@ -24,25 +30,19 @@ function News(){
             <div className="w-full py-16 px-4 text-white font-Poppins ">
 
             </div>
-            <div className="lg:grid lg:grid-cols-2 items-start flex-col-reverse justify-items-center flex gap-6 lg:gap-0 max-w-[1240px] mx-auto">
-                <Card   img={defaultBackground}
+            <div className="lg:grid lg:grid-cols-2 items-start flex-col justify-center flex gap-6 lg:gap-6 max-w-[1240px] mx-auto">
+                
+                {/* Future posts go up */}
+                
+                <Card   className="mx-auto"
+                
+                        img={defaultBackground}
                         title="First Post!" 
-                        author={Izzy} 
-                        blurb="This is the very first post on our site! Hopefully it will be one of many" 
+                        author={Crosby} 
+                        blurb="This is the very first post on our site! Hopefully it will be one of many. I cannot wait to see how this program develops!" 
                         date="June 18, 2022"
                         url="/blog/1"
                         />
-                
-                
-                <Card   img={defaultBackground}
-
-                        title="First Post!" 
-                        author={Izzy} 
-                        blurb="This is the very first post on our site! Hopefully it will be one of many" 
-                        date="June 19, 2022"/>
-                
-                
-                
             </div>
 
         </div>

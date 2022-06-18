0198625c7a47dd1bd66347180d4058730e52c96b
Crosby <gitEmailRespo@gmail.com>
2022-06-18 01:03:28 -0700

    Form logic setup and page redirect; almost done!

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,100 +1,141 @@
-import React from "react";
-
+import React, {useRef} from "react";
+import emailjs from "emailjs-com";
+import { useNavigate } from "react-router-dom";
 const Form = () => {
+    const form = useRef();
+    let navigate = useNavigate();
+    function sendEmail(e) 
+        {
+        
+            e.preventDefault();
+
+            emailjs.sendForm('service_s7vj9tl', 'template_e9an973', form.current, 'eGqu2Ao9HuE-t0Hk-')
+              .then((result) => {
+                  console.log(result.text);
+              }, (error) => {
+                  console.log(error.text);
+              });
+              
+            e.target.reset();
+            navigate("../", { replace: true });
+          }
+
     return(
-<div class="w-4/5 md:w-100 md:max-w-full mx-auto">
-  <div class="p-6 outline rounded-lg outline-pink outline-4 sm:rounded-lg max-w-[700px] mx-auto bg-gray">
-    <form>
-      <label class="block mb-6">
-        <span class="text-gray-700">Your name</span>
-        <input
-          type="text"
-          name="name"
-          class="
-          text-black
-            block
-            w-full
-            mt-1
-            outline
-            outline-navBar
-            rounded-md
-            shadow-sm
-            focus:outline-green
-            focus:ring
-            focus:ring-light-green
-            focus:ring-opacity-50
-          "
-          placeholder="Jane Doe"
-          required
-        />
-      </label>
-      <label class="block mb-6">
-        <span class="text-gray-700">Email address</span>
-        <input
-          name="email"
-          type="email"
-          class="
-         text-black
-            block
-            w-full
-            mt-1
-            outline-navBar
-            rounded-md
-            shadow-sm
-            focus:outline-green
-            focus:ring
-            focus:ring-light-green
-            focus:ring-opacity-50
-          "
-          placeholder="janedoe@example.com"
-          required
-        />
-      </label>
-      <label class="block mb-6">
-        <span class="text-gray-700">Message</span>
-        <textarea
-          name="message"
-          class="
-            text-black
-            block
-            w-full
-            mt-1
-            outline-navBar
-            rounded-md
-            shadow-sm
-            focus:outline-green
-            focus:ring
-            focus:ring-light-green
-            focus:ring-opacity-50
-          "
-          rows="3"
-          placeholder="Let us know any questions you have about the program and when your child would be free for tutoring! We'd also love to know how you heard about us!"
-          required
-        ></textarea>
-      </label>
-      <div class="mb-6">
-        <button
-          type="submit"
-          class="
-            
-            h-10
-            px-5
-            text-black
-            bg-pink
-            rounded-lg
-            transition-colors
-            duration-700
-            focus:shadow-outline
-            hover:scale-110
-            hover:bg-green
-          "
-        >
-          Contact Us
-        </button>
-      </div>
-    </form>
-  </div>
-</div>
+        <div className="w-4/5 md:w-100 md:max-w-full mx-auto">
+            <div className="p-6 outline rounded-lg outline-pink outline-4 sm:rounded-lg max-w-[700px] mx-auto bg-gray">
+                <form onSubmit={sendEmail} ref={form}>
+                    <label className="block mb-6">
+                         <span className="text-gray-700">Your name</span>
+                            <input
+                                type="text"
+                                 name="name"
+                                className="
+                                text-black
+                                   block
+                                    w-full
+                                    mt-1
+                                     outline
+                                    outline-navBar
+                                     rounded-md
+                                    shadow-sm
+                                    focus:outline-green
+                                    focus:ring
+                                    focus:ring-light-green
+                                     focus:ring-opacity-50"
+                                    placeholder="Jane Doe"
+                                    required
+                                     />
+                    </label>
+
+                    <label className="block mb-6">
+                        <span className="text-gray-700">Email address</span>
+                             <input
+                                 name="email"
+                                type="email"
+                                className="
+                                    text-black
+                                    block
+                                    w-full
+                                    outline-navBar
+                                    rounded-md
+                                    shadow-sm
+                                    focus:outline-green
+                                    focus:ring
+                                    focus:ring-light-green
+                                    focus:ring-opacity-50"
+                                    mt-1
+                                    required
+                                    placeholder="janedoe@example.com"
+                                    />
+                    </label>
+
+                    <label className="block mb-6">
+                        <span className="text-gray-700">Message</span>
+                            <textarea
+                                name="message"
+                                className="
+                                    block
+                                    text-black
+                                    mt-1
+                                    w-full
+                                    rounded-md
+                                    shadow-sm
+                                    focus:outline-green
+                                    focus:ring
+                                    focus:ring-light-green
+                                    focus:ring-opacity-50"
+                                    outline-navBar
+                                    placeholder="Let us know any questions you have about the program; we'll go into further talks after the form, but give us a general idea of times and preferences that work for you"
+                                    rows="5"
+                                    required
+                            >
+                            </textarea>
+                    </label>
+
+                    <label className="block mb-6">
+                        <span className="text-gray-700">How'd you hear about us?</span>
+                            <textarea
+                                name="hear"
+                                className="
+                                    text-black
+                                    w-full
+                                    block
+                                    outline-navBar
+                                    mt-1
+                                    rounded-md
+                                    shadow-sm
+                                    focus:outline-green
+                                    focus:ring
+                                    focus:ring-light-green
+                                    focus:ring-opacity-50"
+                                    placeholder="We'd love to know how you heard about us"
+                                    rows="2"
+                                    required
+                            >
+                            </textarea>
+                    </label>
+                    <div className="mb-6">
+                        <button
+                            type="submit"
+                            value="Send"
+                            className="
+                                h-10
+                                px-5
+                                text-black
+                                bg-pink
+                                rounded-lg
+                                transition-colors
+                                duration-700
+                                focus:shadow-outline
+                                hover:scale-110
+                                hover:bg-green"                 
+                        >
+                            Contact Us
+                        </button>
+                    </div>
+                </form>
+            </div>
+        </div>
     )
 }
 

e48642ff7102296f903e509bc48bd0af8f29d2e1
Crosby <gitEmailRespo@gmail.com>
2022-06-18 21:39:31 -0700

    fix offset

--- src/components/contactPage/ContactFooter.jsx
+++ src/components/contactPage/ContactFooter.jsx
@@ -6,7 +6,7 @@ const ContactFooter = () =>
             <div className="w-full mt-20 mx-auto justify-center flex h-28">
                 <div>
                     <h1 className="text-center text-lg font-semibold">Interested in learning more about the team?</h1>
-                    <div className="flex flex-wrap mx-auto text-center px-10">
+                    <div className="flex mx-auto text-center px-10">
                         <p className="text-offwhite">To learn more about the team </p>
                         <a href="/team" className="text-green px-1">click here</a>
                     </div>

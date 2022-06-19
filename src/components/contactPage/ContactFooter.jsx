1ed6994526fdd935224604264c3dd07ab313fc33
Crosby <gitEmailRespo@gmail.com>
2022-06-18 21:50:58 -0700

    fix text fit

--- src/components/contactPage/ContactFooter.jsx
+++ src/components/contactPage/ContactFooter.jsx
@@ -7,8 +7,8 @@ const ContactFooter = () =>
                 <div>
                     <h1 className="text-center md:text-lg text-xs font-semibold px-4">Interested in learning more about the team?</h1>
                     <div className="flex-row flex mx-auto text-center px-10">
-                        <p className="text-offwhite">To learn more about the team </p>
-                        <a href="/team" className="text-green px-1">click here</a>
+                        <p className="text-offwhite text-xs">To learn more about the team </p>
+                        <a href="/team" className="text-green px-1 text-xs">click here</a>
                     </div>
                 </div>
             </div>

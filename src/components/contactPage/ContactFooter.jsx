d98d9f1b20ae6d44d68e4bba4befdd6e207f1924
Crosby <gitEmailRespo@gmail.com>
2022-06-18 21:43:05 -0700

    now text fix ;(

--- src/components/contactPage/ContactFooter.jsx
+++ src/components/contactPage/ContactFooter.jsx
@@ -5,8 +5,8 @@ const ContactFooter = () =>
         return(
             <div className="w-full mt-20 mx-auto justify-center flex h-28">
                 <div>
-                    <h1 className="text-center text-lg font-semibold">Interested in learning more about the team?</h1>
-                    <div className="flex mx-auto text-center px-10">
+                    <h1 className="text-center md:text-lg text-base font-semibold px-4">Interested in learning more about the team?</h1>
+                    <div className="flex-row flex mx-auto text-center px-10">
                         <p className="text-offwhite">To learn more about the team </p>
                         <a href="/team" className="text-green px-1">click here</a>
                     </div>
